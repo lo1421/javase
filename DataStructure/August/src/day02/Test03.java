63dc1b4df216879e6c2460eecda933dc5eefe1f2
logic <1421499510@qq.com>
2023-08-05 00:13:29 +0800

    Test03

--- DataStructure/August/src/day02/Test03.java
+++ DataStructure/August/src/day02/Test03.java
@@ -4,11 +4,15 @@ import java.util.Locale;
 
 public class Test03 {
     public static void main(String[] args) {
-
+    //测试toLowerCase()方法
+        String originalString = args[0]+args[1];
+        String lowerCaseString = originalString.toLowerCase();
+        System.out.println("originalString:"+originalString);
+        System.out.println("lowerCaseString:"+lowerCaseString);
     }
     public static boolean isPalindrome(String s){
      //将大小写统一起来
-     s = s.toLowerCase();
+     s = s.toLowerCase();//将传进来的字符串全部转换为小写后储存在新字符串中并返回这个新字符串
      int left = 0,right = s.length()-1;
      while(left<right){
          //1、从左侧找到一个有效的字符
@@ -29,9 +33,10 @@ public class Test03 {
      return true;
     }
     public static boolean isValidChar(char ch){
-        if(ch>='a'&&ch<='z'||(ch>='0'&&ch<='a')){
+        if(ch>='a'&&ch<='z'||(ch>='0'&&ch<='9')){
             return true;
         }
         return  false;
     }
+
 }
