957871a3066098e348b8e497665951f704721019
logic <1421499510@qq.com>
2023-08-16 23:16:08 +0800

    day04.Test03

---
+++ DataStructure/August/src/day08/Test.java
@@ -0,0 +1,11 @@
+package day08;
+
+public class Test {
+    public static void main(String[] args) {
+        //先创建对象
+        TestSingleList testSingleList =new TestSingleList();
+        testSingleList.createList();
+        testSingleList.display();
+        System.out.println("==========================");
+    }
+}
