81e0d77e36a5d5a882d4a0eb851a232fe61724d3
logic <1421499510@qq.com>
2023-08-21 23:45:02 +0800

    day09/MyLinkedList.java

---
+++ DataStructure/August/src/day09/MyLinkedList.java
@@ -0,0 +1,24 @@
+package day09;
+/*
+这个类是用来实现我的单向不带头非循环的链表数据结构
+ */
+public class MyLinkedList{
+//    头插法
+//    尾插法
+//    任意位置插入 第一个数据节点为0号下标
+//    查找关键字key是否在单链表中
+//    删除第一次出现关键字为key的节点
+//    删除所有值为key的节点
+//    得到单链表的长度
+//    遍历单链表
+//    清空链表
+
+}
+class Node{
+    Node next;
+    Node head;
+    int var;
+    public Node(int var){
+        this.var = var;
+    }
+}
